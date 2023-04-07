f9cf24a13331f1d97ff5a3851da36975b0a263e2
uturuncoglu <turuncu@ucar.edu>
2023-04-06 22:57:13 -0600

    update meta file

--- drivers/ccpp/update.sh
+++ drivers/ccpp/update.sh
@@ -7,6 +7,9 @@ do
   md5sum ../../../../FV3/ccpp/physics/physics/$i $i
 done
 
+cp -f ../../../../FV3/ccpp/physics/physics/noahmpdrv.meta .
+md5sum ../../../../FV3/ccpp/physics/physics/noahmpdrv.meta noahmpdrv.meta
+
 cp -f ../../../../FV3/ccpp/physics/physics/module_sf_noahmp_glacier.F90 ../../src/.
 md5sum ../../../../FV3/ccpp/physics/physics/module_sf_noahmp_glacier.F90 ../../src/module_sf_noahmp_glacier.F90
 cp -f ../../../../FV3/ccpp/physics/physics/module_sf_noahmplsm.F90 ../../src/.
