be0be26c355c4c27c81a5603ef2a4e37ad2f8216
cenlinhe <cenlinhe@ucar.edu>
2024-01-04 20:26:23 -0700

    bug fix for LIS albedo output

--- drivers/lis/EnergyVarOutTransferMod.F90
+++ drivers/lis/EnergyVarOutTransferMod.F90
@@ -136,13 +136,11 @@ contains
     NoahmpIO%CHB2XY  (I,J) = noahmp%energy%state%ExchCoeffSh2mBare
     NoahmpIO%Q2MVXY  (I,J) = noahmp%energy%state%SpecHumidity2mVeg /(1.0-noahmp%energy%state%SpecHumidity2mVeg)  ! spec humidity to mixing ratio
     NoahmpIO%Q2MBXY  (I,J) = noahmp%energy%state%SpecHumidity2mBare/(1.0-noahmp%energy%state%SpecHumidity2mBare)
+    NoahmpIO%ALBEDO  (I,J) = noahmp%energy%state%AlbedoSfc
     NoahmpIO%IRRSPLH (I,J) = NoahmpIO%IRRSPLH(I,J) + &
                              (noahmp%energy%flux%HeatLatentIrriEvap * noahmp%config%domain%MainTimeStep)
     NoahmpIO%TSLB    (I,1:NumSoilLayer,J)       = noahmp%energy%state%TemperatureSoilSnow(1:NumSoilLayer)
     NoahmpIO%TSNOXY  (I,-NumSnowLayerMax+1:0,J) = noahmp%energy%state%TemperatureSoilSnow(-NumSnowLayerMax+1:0)
-    if ( noahmp%energy%state%AlbedoSfc > -999 ) then
-       NoahmpIO%ALBEDO(I,J) = noahmp%energy%state%AlbedoSfc
-    endif
 
     ! New Calculation of total Canopy/Stomatal Conductance Based on Bonan et al. (2011), Inverse of Canopy Resistance (below)
     LeafAreaIndSunlit      = max(noahmp%energy%state%LeafAreaIndSunlit, 0.0)
