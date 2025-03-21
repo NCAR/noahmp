4e4676a55ae2e63e7cef3d18331037d884c08743
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2025-03-21 02:59:46 -0500

    a more cleaner interface for multi-block implementation

--- drivers/erf/NoahmpIO.cpp
+++ drivers/erf/NoahmpIO.cpp
@@ -1,71 +1,67 @@
 #include <NoahmpIO.H>
 #include <NoahArray.H>
 
-void NoahmpIOVarInitDefault(NoahmpIO_type* noahmpio) {
+extern "C" {
+    void NoahmpIOScalarInitDefault_fi(NoahmpIO_type_fi* noahmpio);
+    void NoahmpIOVarInitDefault_fi(NoahmpIO_type_fi* noahmpio);
+    void NoahmpInitMain_fi(NoahmpIO_type_fi* noahmpio);
+    void NoahmpReadNamelist_fi(NoahmpIO_type_fi* noahmpio);
+    void NoahmpReadTable_fi(NoahmpIO_type_fi* noahmpio);
+    void NoahmpReadLandHeader_fi(NoahmpIO_type_fi* noahmpio);
+    void NoahmpReadLandMain_fi(NoahmpIO_type_fi* noahmpio);
+    void NoahmpDriverMain_fi(NoahmpIO_type_fi* noahmpio);
+    void NoahmpIOTypeVectInit_fi(int* NBlocks);
+}
 
-      NoahmpIO_type_fi* fptr = &noahmpio->fptr;
-      NoahmpIOVarInitDefault_fi(fptr);
+void NoahmpIO_type::ScalarInitDefault() {
+     NoahmpIOScalarInitDefault_fi(&fptr);
+};
 
-      noahmpio->XLAT = NoahArray2D<double>(fptr->XLAT, 
-                                          {noahmpio->xstart, noahmpio->ystart}, 
-                                          {noahmpio->xend, noahmpio->yend});
 
-      noahmpio->WSLAKEXY = NoahArray2D<double>(fptr->WSLAKEXY, 
-                                              {noahmpio->xstart, noahmpio->ystart}, 
-                                              {noahmpio->xend, noahmpio->yend});
+void NoahmpIO_type::InitMain() {
+     NoahmpInitMain_fi(&fptr);
+};
 
-      noahmpio->T_PHY = NoahArray3D<double>(fptr->T_PHY, 
-                                           {noahmpio->xstart, noahmpio->kds, noahmpio->ystart}, 
-                                           {noahmpio->xend, noahmpio->kde, noahmpio->yend});
+void NoahmpIO_type::ReadNamelist() {
+     NoahmpReadNamelist_fi(&fptr);
+};
 
-      noahmpio->U_PHY = NoahArray3D<double>(fptr->U_PHY, 
-                                           {noahmpio->xstart, noahmpio->kds, noahmpio->ystart}, 
-                                           {noahmpio->xend, noahmpio->kde, noahmpio->yend});
+void NoahmpIO_type::ReadTable() {
+     NoahmpReadTable_fi(&fptr);
+};
 
-      noahmpio->V_PHY = NoahArray3D<double>(fptr->V_PHY, 
-                                           {noahmpio->xstart, noahmpio->kds, noahmpio->ystart}, 
-                                           {noahmpio->xend, noahmpio->kde, noahmpio->yend});
+void NoahmpIO_type::ReadLandHeader() {
+     NoahmpReadLandHeader_fi(&fptr);
+};
 
-      noahmpio->QV_CURR = NoahArray3D<double>(fptr->QV_CURR, 
-                                             {noahmpio->xstart, noahmpio->kds, noahmpio->ystart}, 
-                                             {noahmpio->xend, noahmpio->kde, noahmpio->yend});
+void NoahmpIO_type::ReadLandMain() {
+     NoahmpReadLandMain_fi(&fptr);
+};
 
-      noahmpio->SHBXY = NoahArray2D<double>(fptr->SHBXY, 
-                                            {noahmpio->xstart, noahmpio->ystart}, 
-                                            {noahmpio->xend, noahmpio->yend});
+void NoahmpIO_type::DriverMain() {
+     NoahmpDriverMain_fi(&fptr);
+};
 
-      noahmpio->EVBXY = NoahArray2D<double>(fptr->EVBXY, 
-                                            {noahmpio->xstart, noahmpio->ystart}, 
-                                            {noahmpio->xend, noahmpio->yend});
+
+void NoahmpIO_type::VarInitDefault() {
+
+      NoahmpIOVarInitDefault_fi(&fptr);
+
+      XLAT     = NoahArray2D<double>(fptr.XLAT,     {xstart,ystart}, {xend,yend});
+      WSLAKEXY = NoahArray2D<double>(fptr.WSLAKEXY, {xstart,ystart}, {xend,yend});
+
+      T_PHY   = NoahArray3D<double>(fptr.T_PHY,   {xstart,kds,ystart}, {xend,kde,yend});
+      U_PHY   = NoahArray3D<double>(fptr.U_PHY,   {xstart,kds,ystart}, {xend,kde,yend});
+      V_PHY   = NoahArray3D<double>(fptr.V_PHY,   {xstart,kds,ystart}, {xend,kde,yend});
+      QV_CURR = NoahArray3D<double>(fptr.QV_CURR, {xstart,kds,ystart}, {xend,kde,yend});
+
+      SHBXY = NoahArray2D<double>(fptr.SHBXY, {xstart,ystart}, {xend,yend});
+      EVBXY = NoahArray2D<double>(fptr.EVBXY, {xstart,ystart}, {xend,yend});
 
 };
 
-void NoahmpInitMain(NoahmpIO_type* noahmpio) {
-     NoahmpIO_type_fi* fptr = &noahmpio->fptr;
-     NoahmpInitMain_fi(fptr);
-};
-
-void NoahmpReadNamelist(NoahmpIO_type* noahmpio) {
-     NoahmpIO_type_fi* fptr = &noahmpio->fptr;
-     NoahmpReadNamelist_fi(fptr);
-};
-
-void NoahmpReadTable(NoahmpIO_type* noahmpio) {
-     NoahmpIO_type_fi* fptr = &noahmpio->fptr;
-     NoahmpReadTable_fi(fptr);
-};
-
-void NoahmpReadLandHeader(NoahmpIO_type* noahmpio) {
-     NoahmpIO_type_fi* fptr = &noahmpio->fptr;
-     NoahmpReadLandHeader_fi(fptr);
-};
-
-void NoahmpReadLandMain(NoahmpIO_type* noahmpio) {
-     NoahmpIO_type_fi* fptr = &noahmpio->fptr;
-     NoahmpReadLandMain_fi(fptr);
-};
-
-void NoahmpDriverMain(NoahmpIO_type* noahmpio) {
-     NoahmpIO_type_fi* fptr = &noahmpio->fptr;
-     NoahmpDriverMain_fi(fptr);
+void NoahmpIO_vector::resize(size_t size) {
+     std::vector<NoahmpIO_type>::resize(size);
+     int _size = size;
+     NoahmpIOTypeVectInit_fi(&_size);
 };
--- drivers/erf/NoahmpIO_fi.F90
+++ drivers/erf/NoahmpIO_fi.F90
@@ -8,14 +8,14 @@ module NoahmpIO_fi
   use NoahmpReadLandMod, ONLY: NoahmpReadLandHeader, NoahmpReadLandMain
   use NoahmpDriverMainMod, ONLY: NoahmpDriverMain
 
-  use, intrinsic :: iso_c_binding
+  use  iso_c_binding
 
   implicit none
 
   ! ---------------------------------------------------------------------------
   ! Public variable of NoahmpIO_type
   ! --------------------------------------------------------------------------- 
-  type(NoahmpIO_type), save, target, public :: NoahmpIO
+  type(NoahmpIO_type), save, target, public, allocatable, dimension(:) :: NoahmpIO
 
   ! ---------------------------------------------------------------------------
   ! Mirror of extern C struct
@@ -37,7 +37,7 @@ module NoahmpIO_fi
     type(C_PTR) :: xstart, xend, ystart, yend
     type(C_PTR) :: nsoil, nsnow
     type(C_PTR) :: itimestep, ntime
-    type(C_PTR) :: llanduse, rank
+    type(C_PTR) :: llanduse, rank, blkid
     type(C_PTR) :: XLAT, WSLAKEXY
     type(C_PTR) :: U_PHY, T_PHY, V_PHY, QV_CURR
     type(C_PTR) :: SHBXY, EVBXY
@@ -45,107 +45,131 @@ module NoahmpIO_fi
 
 contains
 
-  subroutine NoahmpIOTypeInit_fi(NoahmpIO_cptr) bind(C, name="NoahmpIOTypeInit_fi")
-    use, intrinsic :: iso_c_binding
+  subroutine NoahmpIOScalarInitDefault_fi(NoahmpIO_cptr) bind(C, name="NoahmpIOScalarInitDefault_fi")
+    use  iso_c_binding, only : C_INT
     implicit none
     type(NoahmpIO_type_fi), intent(inout) :: NoahmpIO_cptr
+    integer(C_INT), pointer :: bid
+    
+    call C_F_POINTER(NoahmpIO_cptr%BLKID, bid)
 
-    call C_F_POINTER(NoahmpIO_cptr%XSTART, NoahmpIO%XSTART)
-    call C_F_POINTER(NoahmpIO_cptr%XEND, NoahmpIO%XEND)
-    call C_F_POINTER(NoahmpIO_cptr%YSTART, NoahmpIO%YSTART)
-    call C_F_POINTER(NoahmpIO_cptr%YEND, NoahmpIO%YEND)
+    call C_F_POINTER(NoahmpIO_cptr%XSTART, NoahmpIO(bid)%XSTART)
+    call C_F_POINTER(NoahmpIO_cptr%XEND, NoahmpIO(bid)%XEND)
+    call C_F_POINTER(NoahmpIO_cptr%YSTART, NoahmpIO(bid)%YSTART)
+    call C_F_POINTER(NoahmpIO_cptr%YEND, NoahmpIO(bid)%YEND)
 
-    call C_F_POINTER(NoahmpIO_cptr%NSOIL, NoahmpIO%NSOIL)
-    call C_F_POINTER(NoahmpIO_cptr%NSNOW, NoahmpIO%NSNOW)
+    call C_F_POINTER(NoahmpIO_cptr%NSOIL, NoahmpIO(bid)%NSOIL)
+    call C_F_POINTER(NoahmpIO_cptr%NSNOW, NoahmpIO(bid)%NSNOW)
 
-    call C_F_POINTER(NoahmpIO_cptr%IDS, NoahmpIO%IDS)
-    call C_F_POINTER(NoahmpIO_cptr%IDE, NoahmpIO%IDE)
-    call C_F_POINTER(NoahmpIO_cptr%JDS, NoahmpIO%JDS)
-    call C_F_POINTER(NoahmpIO_cptr%JDE, NoahmpIO%JDE)
-    call C_F_POINTER(NoahmpIO_cptr%KDS, NoahmpIO%KDS)
-    call C_F_POINTER(NoahmpIO_cptr%KDE, NoahmpIO%KDE)
+    call C_F_POINTER(NoahmpIO_cptr%IDS, NoahmpIO(bid)%IDS)
+    call C_F_POINTER(NoahmpIO_cptr%IDE, NoahmpIO(bid)%IDE)
+    call C_F_POINTER(NoahmpIO_cptr%JDS, NoahmpIO(bid)%JDS)
+    call C_F_POINTER(NoahmpIO_cptr%JDE, NoahmpIO(bid)%JDE)
+    call C_F_POINTER(NoahmpIO_cptr%KDS, NoahmpIO(bid)%KDS)
+    call C_F_POINTER(NoahmpIO_cptr%KDE, NoahmpIO(bid)%KDE)
 
-    call C_F_POINTER(NoahmpIO_cptr%IMS, NoahmpIO%IMS)
-    call C_F_POINTER(NoahmpIO_cptr%IME, NoahmpIO%IME)
-    call C_F_POINTER(NoahmpIO_cptr%JMS, NoahmpIO%JMS)
-    call C_F_POINTER(NoahmpIO_cptr%JME, NoahmpIO%JME)
-    call C_F_POINTER(NoahmpIO_cptr%KMS, NoahmpIO%KMS)
-    call C_F_POINTER(NoahmpIO_cptr%KME, NoahmpIO%KME)
+    call C_F_POINTER(NoahmpIO_cptr%IMS, NoahmpIO(bid)%IMS)
+    call C_F_POINTER(NoahmpIO_cptr%IME, NoahmpIO(bid)%IME)
+    call C_F_POINTER(NoahmpIO_cptr%JMS, NoahmpIO(bid)%JMS)
+    call C_F_POINTER(NoahmpIO_cptr%JME, NoahmpIO(bid)%JME)
+    call C_F_POINTER(NoahmpIO_cptr%KMS, NoahmpIO(bid)%KMS)
+    call C_F_POINTER(NoahmpIO_cptr%KME, NoahmpIO(bid)%KME)
 
-    call C_F_POINTER(NoahmpIO_cptr%ITS, NoahmpIO%ITS)
-    call C_F_POINTER(NoahmpIO_cptr%ITE, NoahmpIO%ITE)
-    call C_F_POINTER(NoahmpIO_cptr%JTS, NoahmpIO%JTS)
-    call C_F_POINTER(NoahmpIO_cptr%JTE, NoahmpIO%JTE)
-    call C_F_POINTER(NoahmpIO_cptr%KTS, NoahmpIO%KTS)
-    call C_F_POINTER(NoahmpIO_cptr%KTE, NoahmpIO%KTE)
+    call C_F_POINTER(NoahmpIO_cptr%ITS, NoahmpIO(bid)%ITS)
+    call C_F_POINTER(NoahmpIO_cptr%ITE, NoahmpIO(bid)%ITE)
+    call C_F_POINTER(NoahmpIO_cptr%JTS, NoahmpIO(bid)%JTS)
+    call C_F_POINTER(NoahmpIO_cptr%JTE, NoahmpIO(bid)%JTE)
+    call C_F_POINTER(NoahmpIO_cptr%KTS, NoahmpIO(bid)%KTS)
+    call C_F_POINTER(NoahmpIO_cptr%KTE, NoahmpIO(bid)%KTE)
 
-    call C_F_POINTER(NoahmpIO_cptr%ITIMESTEP, NoahmpIO%ITIMESTEP)
-    call C_F_POINTER(NoahmpIO_cptr%NTIME, NoahmpIO%NTIME)
+    call C_F_POINTER(NoahmpIO_cptr%ITIMESTEP, NoahmpIO(bid)%ITIMESTEP)
+    call C_F_POINTER(NoahmpIO_cptr%NTIME, NoahmpIO(bid)%NTIME)
 
-    call C_F_POINTER(NoahmpIO_cptr%LLANDUSE, NoahmpIO%LLANDUSE)
-    NoahmpIO%LLANDUSE => NoahmpIO%LLANDUSE(1:256)
+    call C_F_POINTER(NoahmpIO_cptr%LLANDUSE, NoahmpIO(bid)%LLANDUSE)
+    NoahmpIO(bid)%LLANDUSE => NoahmpIO(bid)%LLANDUSE(1:256)
 
-    call C_F_POINTER(NoahmpIO_cptr%RANK, NoahmpIO%RANK)
-  end subroutine NoahmpIOTypeInit_fi
+    call C_F_POINTER(NoahmpIO_cptr%RANK, NoahmpIO(bid)%RANK)
+  end subroutine NoahmpIOScalarInitDefault_fi
 
   subroutine NoahmpIOVarInitDefault_fi(NoahmpIO_cptr) bind(C, name="NoahmpIOVarInitDefault_fi")
-    use, intrinsic :: iso_c_binding
+    use  iso_c_binding, only : C_INT
     implicit none
     type(NoahmpIO_type_fi), intent(inout) :: NoahmpIO_cptr
-
-    call NoahmpIOVarInitDefault(NoahmpIO)
-
-    NoahmpIO_cptr%XLAT = C_LOC(NoahmpIO%XLAT)
-    NoahmpIO_cptr%WSLAKEXY = C_LOC(NoahmpIO%WSLAKEXY)
+    integer(C_INT), pointer :: bid
     
-    NoahmpIO_cptr%T_PHY = C_LOC(NoahmpIO%T_PHY)
-    NoahmpIO_cptr%U_PHY = C_LOC(NoahmpIO%U_PHY)
-    NoahmpIO_cptr%V_PHY = C_LOC(NoahmpIO%V_PHY)
-    NoahmpIO_cptr%QV_CURR = C_LOC(NoahmpIO%QV_CURR)
-    NoahmpIO_cptr%SHBXY = C_LOC(NoahmpIO%SHBXY)
-    NoahmpIO_cptr%EVBXY = C_LOC(NoahmpIO%EVBXY)
+    call C_F_POINTER(NoahmpIO_cptr%BLKID, bid)
+
+    call NoahmpIOVarInitDefault(NoahmpIO(bid))
+
+    NoahmpIO_cptr%XLAT = C_LOC(NoahmpIO(bid)%XLAT)
+    NoahmpIO_cptr%WSLAKEXY = C_LOC(NoahmpIO(bid)%WSLAKEXY)
+    NoahmpIO_cptr%T_PHY = C_LOC(NoahmpIO(bid)%T_PHY)
+    NoahmpIO_cptr%U_PHY = C_LOC(NoahmpIO(bid)%U_PHY)
+    NoahmpIO_cptr%V_PHY = C_LOC(NoahmpIO(bid)%V_PHY)
+    NoahmpIO_cptr%QV_CURR = C_LOC(NoahmpIO(bid)%QV_CURR)
+    NoahmpIO_cptr%SHBXY = C_LOC(NoahmpIO(bid)%SHBXY)
+    NoahmpIO_cptr%EVBXY = C_LOC(NoahmpIO(bid)%EVBXY)
   end subroutine NoahmpIOVarInitDefault_fi
 
   subroutine NoahmpInitMain_fi(NoahmpIO_cptr) bind(C, name="NoahmpInitMain_fi")
-    use, intrinsic :: iso_c_binding 
+    use  iso_c_binding, only : C_INT
     implicit none 
     type(NoahmpIO_type_fi), intent(inout) :: NoahmpIO_cptr
-    call NoahmpInitMain(NoahmpIO)
+    integer(C_INT), pointer :: bid    
+    call C_F_POINTER(NoahmpIO_cptr%BLKID, bid)
+    call NoahmpInitMain(NoahmpIO(bid))
   end subroutine NoahmpInitMain_fi
 
   subroutine NoahmpReadTable_fi(NoahmpIO_cptr) bind(C, name="NoahmpReadTable_fi")
-    use, intrinsic :: iso_c_binding 
+    use  iso_c_binding, only : C_INT
     implicit none 
     type(NoahmpIO_type_fi), intent(inout) :: NoahmpIO_cptr
-    call NoahmpReadTable(NoahmpIO)
+    integer(C_INT), pointer :: bid    
+    call C_F_POINTER(NoahmpIO_cptr%BLKID, bid)
+    call NoahmpReadTable(NoahmpIO(bid))
   end subroutine NoahmpReadTable_fi
 
   subroutine NoahmpReadNamelist_fi(NoahmpIO_cptr) bind(C, name="NoahmpReadNamelist_fi")
-    use, intrinsic :: iso_c_binding 
+    use  iso_c_binding, only : C_INT 
     implicit none 
     type(NoahmpIO_type_fi), intent(inout) :: NoahmpIO_cptr
-    call NoahmpReadNamelist(NoahmpIO)
+    integer(C_INT), pointer :: bid
+    call C_F_POINTER(NoahmpIO_cptr%BLKID, bid)
+    call NoahmpReadNamelist(NoahmpIO(bid))
   end subroutine NoahmpReadNamelist_fi
 
   subroutine NoahmpReadLandHeader_fi(NoahmpIO_cptr) bind(C, name="NoahmpReadLandHeader_fi")
-    use, intrinsic :: iso_c_binding 
+    use  iso_c_binding, only : C_INT 
     implicit none 
     type(NoahmpIO_type_fi), intent(inout) :: NoahmpIO_cptr
-    call NoahmpReadLandHeader(NoahmpIO)
+    integer(C_INT), pointer :: bid
+    call C_F_POINTER(NoahmpIO_cptr%BLKID, bid)
+    call NoahmpReadLandHeader(NoahmpIO(bid))
   end subroutine NoahmpReadLandHeader_fi
 
   subroutine NoahmpReadLandMain_fi(NoahmpIO_cptr) bind(C, name="NoahmpReadLandMain_fi")
-    use, intrinsic :: iso_c_binding 
+    use  iso_c_binding, only : C_INT 
     implicit none 
     type(NoahmpIO_type_fi), intent(inout) :: NoahmpIO_cptr
-    call NoahmpReadLandMain(NoahmpIO)
+    integer(C_INT), pointer :: bid
+    call C_F_POINTER(NoahmpIO_cptr%BLKID, bid)
+    call NoahmpReadLandMain(NoahmpIO(bid))
   end subroutine NoahmpReadLandMain_fi
 
   subroutine NoahmpDriverMain_fi(NoahmpIO_cptr) bind(C, name="NoahmpDriverMain_fi")
-    use, intrinsic :: iso_c_binding 
+    use iso_c_binding, only : C_INT 
     implicit none 
     type(NoahmpIO_type_fi), intent(inout) :: NoahmpIO_cptr
-    call NoahmpDriverMain(NoahmpIO)
+    integer(C_INT), pointer :: bid
+    call C_F_POINTER(NoahmpIO_cptr%BLKID, bid)
+    call NoahmpDriverMain(NoahmpIO(bid))
   end subroutine NoahmpDriverMain_fi
+
+  subroutine NoahmpIOTypeVectInit_fi(NBlocks) bind(C, name="NoahmpIOTypeVectInit_fi")
+    use iso_c_binding, only : C_INT
+    implicit none
+    integer(C_INT), intent(in) :: NBlocks
+    allocate(NoahmpIO(0:NBlocks-1))
+  end subroutine NoahmpIOTypeVectInit_fi
 
 end module NoahmpIO_fi
