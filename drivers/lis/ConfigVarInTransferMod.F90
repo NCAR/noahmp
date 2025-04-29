e4a7ec9d936c83dad8e46c8b339f9dd18e1e79ff
cenlinhe <cenlinhe@ucar.edu>
2025-04-29 10:58:08 -0600

    update solar band number initialization

--- drivers/lis/ConfigVarInTransferMod.F90
+++ drivers/lis/ConfigVarInTransferMod.F90
@@ -78,7 +78,7 @@ contains
 
     ! config domain variable
     noahmp%config%domain%SurfaceType                 = 1
-    noahmp%config%domain%NumSwRadBand                = 2
+    noahmp%config%domain%NumSwRadBand                = NoahmpIO%NUMRAD
     noahmp%config%domain%SoilColor                   = 4
     noahmp%config%domain%NumCropGrowStage            = 8
     noahmp%config%domain%FlagSoilProcess             = NoahmpIO%calculate_soil
