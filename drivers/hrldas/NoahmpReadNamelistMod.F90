a9027172c11b4baf22f57734aab63e29d7e72af1
Tzu-Shun Lin <tslin2@ucar.edu>
2026-05-29 14:31:05 -0700

    Urban fix for urban physics variables allocation and default urban vertical resolution for BEP (#243)
    * fix issue of urban variable allocation
    
    * update erf,lis,and wrf driver interface

--- drivers/hrldas/NoahmpReadNamelistMod.F90
+++ drivers/hrldas/NoahmpReadNamelistMod.F90
@@ -56,7 +56,7 @@ contains
     integer                 :: num_urban_hi     = 15 
     integer                 :: num_urban_ngr    = 10  ! = ngr_u in bep_bem.F
     integer                 :: noahmp_output    = 0
-    real(kind=kind_noahmp)  :: urban_atmosphere_thickness = 2.0
+    real(kind=kind_noahmp)  :: urban_atmosphere_thickness = 5.0
     real(kind=kind_noahmp)  :: soil_timestep    = 0.0   ! soil timestep (default=0: same as main noahmp timestep)
 
     character(len=256)      :: forcing_name_T  = "T2D"
